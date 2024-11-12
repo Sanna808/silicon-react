541683e0043ac237cd4445400407112a1ebe0cae
sanna <sanna.makimaa@utb.ecutbildning.se>
2024-11-12 11:38:05 +0100

    fixat darkmode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LogoTypeLight from '../assets/images/logo.svg'
 import LogoTypeDark from '../assets/images/silicon-dark.svg'
 
 
 const Header = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  useEffect(() => {
+    const storedThemeMode = localStorage.getItem('themeMode')
+
+    if (storedThemeMode === 'dark' || (!storedThemeMode && window.matchMedia('preferes-color-scheme : dark').matches)) {
+      setIsDarkMode(true)
+      document.documentElement.classList.add('dark')
+    } else {
+      setIsDarkMode(false)
+      document.documentElement.classList.remove('dark')
+    }
+  }, [])
+
+  const handleToggle = () => {
+    if (isDarkMode) {
+      setIsDarkMode(false)
+      localStorage.setItem('themeMode', 'light')
+      document.documentElement.classList.remove('dark')
+    } else {
+      setIsDarkMode(true)
+      localStorage.setItem('themeMode', 'dark')
+      document.documentElement.classList.add('dark')
+    }
+  }
+
   return (
     <header>
       <div className="container">
@@ -19,8 +45,8 @@ const Header = () => {
 
         <div id="darkmode-toggle-switch" className="btn-toggle-switch">
           <span className="label">Dark Mode</span> 
-          <label for="darkmode-switch" className="toggle-switch">
-            <input id="darkmode-switch" type="checkbox" />
+          <label htmlFor="darkmode-switch" className="toggle-switch">
+            <input id="darkmode-switch" type="checkbox" checked={isDarkMode} onChange={handleToggle} />
             <span className="slider round"></span>
           </label>
         </div>
